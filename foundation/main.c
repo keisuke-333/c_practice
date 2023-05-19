45e9e73372e310851731f55e9790582367b91a2f
keisuke-333 <keikubo.999@gmail.com>
2023-05-20 04:24:54 +0900

    circle area

---
+++ foundation/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+  float radius = 3.5;
+  float pi = 3.14;
+  float area = pi * radius * radius;
+  printf("Area of the circle is: %f\n", area);
+
+  return 0;
+}
