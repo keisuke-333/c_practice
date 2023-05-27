e6933ff4f70dfb95cf76e9c6259f7ae8d276b0c2
keisuke-333 <keikubo.999@gmail.com>
2023-05-27 16:58:56 +0900

    FILE - binary

--- foundation/main.c
+++ foundation/main.c
@@ -8,7 +8,7 @@ int main()
   printf("Area of the circle is: %f\n", area);
 
   FILE *fp;
-  fp = fopen("file.txt", "w+");
+  fp = fopen("file.txt", "w");
   fprintf(fp, "Hello process B\n");
   fclose(fp);
 
@@ -16,6 +16,17 @@ int main()
   fp = fopen("file.txt", "r");
   fgets(str, 256, fp);
   printf("%s\n", str);
+  fclose(fp);
+
+  int a = 10;
+  fp = fopen("file2.txt", "wb");
+  fwrite(&a, sizeof(a), 1, fp);
+  fclose(fp);
+
+  int b;
+  fp = fopen("file2.txt", "rb");
+  fread(&b, sizeof(b), 1, fp);
+  printf("%d\n", b);
   fclose(fp);
 
   return 0;
