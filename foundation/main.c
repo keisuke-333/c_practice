2125d3248b4f2d01cf00c9640b32d7a6f81d3a93
keisuke-333 <keikubo.999@gmail.com>
2023-05-28 15:43:57 +0900

    socket - client

--- foundation/main.c
+++ foundation/main.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
 
 int main()
 {
@@ -28,6 +34,20 @@ int main()
   fread(&b, sizeof(b), 1, fp);
   printf("%d\n", b);
   fclose(fp);
+
+  struct sockaddr_in server;
+  char buf[256];
+
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+  server.sin_addr.s_addr = inet_addr("127.0.0.1");
+  server.sin_port = htons(9999);
+  server.sin_family = AF_INET;
+
+  connect(sock, (struct sockaddr *)&server, sizeof(server));
+  strcpy(buf, "hello");
+  send(sock, buf, sizeof(buf), 0);
+
+  close(sock);
 
   return 0;
 }
