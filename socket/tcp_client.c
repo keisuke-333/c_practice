71d155f56a566ddc57e9160a29811d054e900868
keisuke-333 <keikubo.999@gmail.com>
2023-05-30 15:33:03 +0900

    tcp comms

---
+++ socket/tcp_client.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+
+int main()
+{
+  struct sockaddr_in server;
+  char buf[256];
+
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+
+  server.sin_addr.s_addr = inet_addr("127.0.0.1");
+  server.sin_port = htons(9999);
+  server.sin_family = AF_INET;
+
+  connect(sock, (struct sockaddr *)&server, sizeof(server));
+
+  strcpy(buf, "hello");
+  send(sock, buf, sizeof(buf), 0);
+
+  close(sock);
+
+  return 0;
+}
